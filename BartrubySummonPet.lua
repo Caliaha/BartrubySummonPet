81aa755a5020af1a886992915b4e08cd9861e83a
nagboy87 <34219663+Caliaha@users.noreply.github.com>
2014-04-16 19:17:39 +0000

    Added ability to disable pet summoning

--- BartrubySummonPet.lua
+++ BartrubySummonPet.lua
@@ -28,19 +28,22 @@ function BartrubySummonPet:OnInitialize()
       tile = true, tileSize = 16, edgeSize = 16, 
       insets = { left = 4, right = 4, top = 4, bottom = 4 }});
  frame:SetBackdropColor(1,1,1,1) ]]--
- frame.texture = frame:CreateTexture(nil, 'HIGH')
+ frame.texture = frame:CreateTexture(nil, 'BACKGROUND')
  frame.texture:SetAllPoints(true)
  frame.texture:SetTexture("Interface\\ICONS\\Trade_Archaeology_TyrandesFavoriteDoll.blp")
+ frame.enabled = frame:CreateTexture(nil, 'HIGH')
+ frame.enabled:SetWidth(32)
+ frame.enabled:SetHeight(32)
+ frame.enabled:SetPoint("BOTTOMRIGHT")
  
  frame:RegisterEvent("OnReceiveDrag")
  frame:RegisterEvent("OnMouseUp")
  frame:SetScript("OnShow", function(self) BartrubySummonPet:PlaceIcon() end)
- frame:SetScript("OnReceiveDrag", function(self) BartrubySummonPet:CheckCursor() end)
- frame:SetScript("OnMouseUp", function(self) BartrubySummonPet:CheckCursor() end)
+ frame:SetScript("OnReceiveDrag", function(self) BartrubySummonPet:CheckCursor(nil) end)
+ frame:SetScript("OnMouseUp", function(self, button) BartrubySummonPet:CheckCursor(button) end)
  frame:EnableMouse(true)
  frame:Show()
- self.bpFrame = frame
- 
+ self.bpFrame = frame 
 end
 
 function BartrubySummonPet:OnEnable()
@@ -53,18 +56,36 @@ function BartrubySummonPet:OnDisable()
  self.bpFrame:Hide()
 end
 
-function BartrubySummonPet:CheckCursor()
+function BartrubySummonPet:CheckCursor(button)
  --self:Print(GetCursorInfo())
  local type, id = GetCursorInfo()
  if (type == "battlepet") then
   self.db.char.battlepet = id
   self:PlaceIcon()
-  ClearCursor()        
+  ClearCursor()
+  return
+ end
+ if (button == "RightButton") then
+  if (IsControlKeyDown()) then
+   if (self.db.char.enabled) then
+    self.db.char.enabled = false
+   else
+    self.db.char.enabled = true
+   end
+  else
+   self.db.char.battlepet = nil
+  end
+  self:PlaceIcon()
  end
 end
 
 function BartrubySummonPet:PlaceIcon()
  -- May need to add checks to see if they pet exists
+ if (self.db.char.enabled) then
+  self.bpFrame.enabled:SetTexture("Interface\\Buttons\\UI-CheckBox-Check")
+ else
+  self.bpFrame.enabled:SetTexture("Interface\\Buttons\\UI-CheckBox-Check-Disabled")
+ end
  local id = self.db.char.battlepet
  if (not id) then
   self.bpFrame.texture:SetTexture("Interface\\ICONS\\Trade_Archaeology_TyrandesFavoriteDoll.blp");
@@ -77,6 +98,7 @@ end
 
 function BartrubySummonPet:SummonPet()
  -- Things to check for: combat, casting, stealth, mounted, eating/drinking
+ if (not self.db.char.enabled) then return end
  if (InCombatLockdown()) then return end
  local id = self.db.char.battlepet
  local currentPet = C_PetJournal.GetSummonedPetGUID()
